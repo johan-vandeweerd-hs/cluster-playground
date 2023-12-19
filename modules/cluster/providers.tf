1092b0d198320033da1e0d633a74cee5768801e4
Johan Vandeweerd <johan.vandeweerd@hootsuite.com>
2023-12-19 21:09:59 +0100

    Replace deprecated kubectl provider

--- modules/cluster/providers.tf
+++ modules/cluster/providers.tf
@@ -7,8 +7,8 @@ terraform {
       version = ">= 5.0"
     }
     kubectl = {
-      source = "gavinbunney/kubectl"
-      version = ">= 1.14"
+      source  = "alekc/kubectl"
+      version = ">= 2.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
