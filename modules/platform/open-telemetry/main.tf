2ae6ad7c8ec2d5572863dca978ed21aee2c9fed5
Johan Vandeweerd <johan.vandeweerd@hootsuite.com>
2023-12-20 17:39:48 +0100

    Rename variables + add dynamic Helm parameters

--- modules/platform/open-telemetry/main.tf
+++ modules/platform/open-telemetry/main.tf
@@ -1,15 +1,15 @@
 locals {
-  moduleName = basename(abspath(path.module))
+  module_name = basename(abspath(path.module))
 }
 
 resource "kubectl_manifest" "application" {
   yaml_body = templatefile("${path.module}/chart/application.yaml", {
-    name           = local.moduleName
-    namespace      = local.moduleName
+    name           = local.module_name
+    namespace      = local.module_name
     gitUrl         = var.git_url
     revision       = var.git_revision
-    helmParameters = {
-      roleArn = aws_iam_role.open_telemetry.arn
+    helm_parameters = {
+      roleArn      = aws_iam_role.open_telemetry.arn
     }
   })
 }
