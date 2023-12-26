340af4e176e96eeb213c5b44ee61a002c36d1aef
Johan Vandeweerd <johan.vandeweerd@hootsuite.com>
2023-12-26 21:16:54 +0100

    Remove ingress-nginx and overprovisioner

--- modules/platform/main.tf
+++ modules/platform/main.tf
@@ -37,34 +37,8 @@ module "external_secrets" {
   cluster_oidc_provider_arn          = var.cluster_oidc_provider_arn
 }
 
-module "ingress_nginx" {
-  source = "./ingress-nginx"
-
-  git_url      = var.git_url
-  git_revision = var.git_revision
-
-  cluster_name                       = var.cluster_name
-  cluster_endpoint                   = var.cluster_endpoint
-  cluster_certificate_authority_data = var.cluster_certificate_authority_data
-  cluster_oidc_provider              = var.cluster_oidc_provider
-  cluster_oidc_provider_arn          = var.cluster_oidc_provider_arn
-}
-
 module "open_telemetry" {
   source = "./open-telemetry"
-
-  git_url      = var.git_url
-  git_revision = var.git_revision
-
-  cluster_name                       = var.cluster_name
-  cluster_endpoint                   = var.cluster_endpoint
-  cluster_certificate_authority_data = var.cluster_certificate_authority_data
-  cluster_oidc_provider              = var.cluster_oidc_provider
-  cluster_oidc_provider_arn          = var.cluster_oidc_provider_arn
-}
-
-module "overprovisioner" {
-  source = "./overprovisioner"
 
   git_url      = var.git_url
   git_revision = var.git_revision
