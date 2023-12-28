a7b6da619e3959dd686a425d6cf534fda6212d1d
Johan Vandeweerd <johan.vandeweerd@hootsuite.com>
2023-12-28 12:25:13 +0100

    Allow for setting a project name to replace the `cluster-playground` name

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 locals {
-  name            = "cluster-playground-${var.contributor}"
+  name            = coalesce(var.project_name, "cluster-playground-${var.contributor}")
   vpc_cidr        = "10.0.0.0/16"
   azs             = ["${data.aws_region.this.name}a", "${data.aws_region.this.name}b", "${data.aws_region.this.name}c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "project_name" {
+  description = "Name to use for the VPC, EKS cluster, etc and to use as prefix to name resources."
+  type = string
+  default = ""
+}
+
 variable "git_url" {
   description = "The Git URL used in the Argpcd application manifests."
   type        = string
