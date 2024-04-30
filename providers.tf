8a33c3b1212ac412033f15cb1412f4d3951e1e03
Johan Vandeweerd <johan.vandeweerd@hootsuite.com>
2024-04-30 15:22:31 +0200

    Move providers configuration from modules to root of project

--- providers.tf
+++ providers.tf
@@ -10,6 +10,14 @@ terraform {
       source  = "alekc/kubectl"
       version = ">= 2.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.24"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.12"
+    }
   }
 }
 
@@ -29,5 +37,29 @@ provider "kubectl" {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
     args        = ["eks", "get-token", "--cluster-name", module.cluster.cluster_name]
+  }
+}
+
+provider "kubernetes" {
+  host                   = module.cluster.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.cluster.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.cluster.cluster_name]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.cluster.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.cluster.cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.cluster.cluster_name]
+    }
   }
 }
