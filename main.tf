2924bcf7b049f03aee56a621610281cc91ad3723
Johan Vandeweerd <johan.vandeweerd@hootsuite.com>
2024-10-12 20:03:55 +0200

    Move VPC configuration to network module

--- main.tf
+++ main.tf
@@ -1,20 +1,12 @@
 locals {
-  name            = var.project_name
-  vpc_cidr        = "10.0.0.0/16"
-  azs             = ["${data.aws_region.this.name}a", "${data.aws_region.this.name}b", "${data.aws_region.this.name}c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  git_revision    = coalesce(var.git_revision, var.project_name)
+  name         = var.project_name
+  git_revision = coalesce(var.git_revision, var.project_name)
 }
 
 module "network" {
   source = "./modules/network"
 
-  name            = local.name
-  vpc_cidr        = local.vpc_cidr
-  azs             = local.azs
-  private_subnets = local.private_subnets
-  public_subnets  = local.public_subnets
+  name = local.name
 }
 
 module "cluster" {
--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,18 +1,25 @@
+locals {
+  vpc_cidr        = "10.0.0.0/16"
+  azs             = ["${data.aws_region.this.name}a", "${data.aws_region.this.name}b", "${data.aws_region.this.name}c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = var.name
 
-  azs = var.azs
+  azs = local.azs
 
-  cidr = var.vpc_cidr
+  cidr = local.vpc_cidr
 
-  private_subnets = var.private_subnets
+  private_subnets = local.private_subnets
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb" = ""
   }
 
-  public_subnets = var.public_subnets
+  public_subnets = local.public_subnets
   public_subnet_tags = {
     "kubernetes.io/role/elb" = ""
   }
