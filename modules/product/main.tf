c1392d73e8632b447bc8a6b420dd77af0942e575
Johan Vandeweerd <johan.vandeweerd@hootsuite.com>
2024-10-14 09:58:49 +0200

    Add test application that lists S3 buckets in the current AWS account and uses pod identity

--- modules/product/main.tf
+++ modules/product/main.tf
@@ -5,3 +5,11 @@ module "httpbin" {
   git_revision = var.git_revision
   project_name = var.project_name
 }
+
+module "bucket_lister" {
+  source = "./bucket-lister"
+
+  git_url      = var.git_url
+  git_revision = var.git_revision
+  project_name = var.project_name
+}
