7d072efec783a00c37e28a11ea877dc9546248e9
Johan Vandeweerd <johan.vandeweerd@hootsuite.com>
2024-10-14 16:41:29 +0200

    Add destroy shell script

---
+++ scripts/destroy.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+terraform apply -target module.platform -destroy -auto-approve
+kubectl delete poddisruptionbudget -A --all
+kubectl delete nodepool --all
+kubectl delete nodeclaims --all
+kubectl delete ec2nodeclass --all
+terraform apply -target module.cluster -destroy -auto-approve
+terraform apply -target module.network -destroy -auto-approve
