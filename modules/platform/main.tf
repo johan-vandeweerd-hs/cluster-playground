e2b6f9e2fedbcd387fe554b139821b225a1ae401
Johan Vandeweerd <johan.vandeweerd@hootsuite.com>
2024-11-18 12:25:17 +0100

    Remove open-telemetry

--- modules/platform/main.tf
+++ modules/platform/main.tf
@@ -49,17 +49,3 @@ module "ingress" {
 
   hosted_zone = var.hosted_zone
 }
-
-module "open_telemetry" {
-  source = "./open-telemetry"
-
-  project_name = var.project_name
-
-  git_url      = var.git_url
-  git_revision = var.git_revision
-
-  kubernetes_oidc_provider     = var.kubernetes_oidc_provider
-  kubernetes_oidc_provider_arn = var.kubernetes_oidc_provider_arn
-
-  hosted_zone = var.hosted_zone
-}
