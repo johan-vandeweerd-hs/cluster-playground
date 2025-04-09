51d78411d541c7f5a0c0580c940fd3a34237bbb9
Johan Vandeweerd <johan.vandeweerd@hootsuite.com>
2025-04-09 18:38:41 +0200

    Update provider versions

--- providers.tf
+++ providers.tf
@@ -1,22 +1,21 @@
 terraform {
-  required_version = ">= 1.5"
-
+  required_version = ">= 1.9"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = "~> 5.0"
     }
     kubectl = {
       source  = "alekc/kubectl"
-      version = ">= 2.0"
+      version = "~> 2.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.24"
+      version = "~> 2.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.12"
+      version = "~> 2.0"
     }
   }
 }
