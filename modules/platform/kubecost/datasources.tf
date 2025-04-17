b4810fa648f6d80a09ee2e22a894d39983b6af18
Johan Vandeweerd <johan.vandeweerd@hootsuite.com>
2025-04-17 14:50:32 +0200

    Kubecost

---
+++ modules/platform/kubecost/datasources.tf
@@ -0,0 +1,8 @@
+data "aws_region" "this" {
+}
+
+data "aws_caller_identity" "this" {
+}
+
+data "aws_default_tags" "this" {
+}
---
+++ modules/platform/kubecost/main.tf
@@ -0,0 +1,11 @@
+# ArgoCD applications
+resource "kubectl_manifest" "application_kubecost" {
+  yaml_body = templatefile("${path.module}/chart/kubecost/application.yaml", {
+    name      = "kubecost"
+    namespace = "kubecost"
+    gitUrl    = var.git_url
+    revision  = var.git_revision
+    helmParameters = merge({ for key, value in data.aws_default_tags.this.tags : "tags.${key}" => value }, {
+    })
+  })
+}
