9c412d64a29fee707ae4a37d8451bd6f8349d601
Johan Vandeweerd <johan.vandeweerd@hootsuite.com>
2026-04-22 12:59:20 +0200

    deploy managed Prometheus and Grafana modules for observability

--- data_sources.tf
+++ data_sources.tf
@@ -27,3 +27,16 @@ data "aws_identitystore_group" "argocd_admin" {
     }
   }
 }
+
+data "aws_identitystore_group" "grafana_admin" {
+  provider = aws.main
+
+  identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
+
+  alternate_identifier {
+    unique_attribute {
+      attribute_path  = "DisplayName"
+      attribute_value = "GrafanaAdmin"
+    }
+  }
+}
---
+++ observability.tf
@@ -0,0 +1,64 @@
+module "prometheus" {
+  source  = "terraform-aws-modules/managed-service-prometheus/aws"
+  version = "~> 4.0"
+
+  workspace_alias = var.project_name
+
+  retention_period_in_days               = 7
+  cloudwatch_log_group_retention_in_days = 7
+
+  create_alert_manager = false
+
+  tags = {
+    AMPAgentlessScraper = ""
+  }
+}
+
+module "grafana" {
+  source  = "terraform-aws-modules/managed-service-grafana/aws"
+  version = "~> 2.0"
+
+  name              = var.project_name
+  description       = "TF: AWS Managed Grafana service for ${var.project_name}"
+  associate_license = false
+  data_sources      = ["PROMETHEUS"]
+
+  iam_role_name            = "${var.project_name}-grafana"
+  use_iam_role_name_prefix = false
+
+  security_group_name            = "${var.project_name}-grafana"
+  security_group_use_name_prefix = false
+  security_group_rules = {
+    outbound = {
+      type        = "egress"
+      description = "TF: Allow all outbound traffic"
+      protocol    = "all"
+      from_port   = 0
+      to_port     = 0
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  security_group_tags = {
+    Name = "${var.project_name}-grafana"
+  }
+
+  vpc_configuration = {
+    subnet_ids = module.vpc.private_subnets
+  }
+
+  configuration = jsonencode({
+    unifiedAlerting = {
+      enabled = false
+    }
+    plugins = {
+      pluginAdminEnabled = true
+    }
+  })
+
+  role_associations = {
+    admin = {
+      role      = "ADMIN"
+      group_ids = [data.aws_identitystore_group.grafana_admin.id]
+    }
+  }
+}
