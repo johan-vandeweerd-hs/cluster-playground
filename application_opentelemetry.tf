5da90bab6ba98b12c384f54d8c3872272c27886a
Johan Vandeweerd <johan.vandeweerd@hootsuite.com>
2026-04-22 13:00:06 +0200

    remove OpenTelemetry logs collector and refine metrics collector configuration

--- application_opentelemetry.tf
+++ application_opentelemetry.tf
@@ -1,43 +1,50 @@
 resource "kubectl_manifest" "argocd_application_opentelemetrycollectors" {
   yaml_body = templatefile("${path.module}/applications/open-telemetry-collectors/argocd-application.yaml", {
+    valuesObject = {
+      aws = {
+        region = data.aws_region.this.id
+      }
+      prometheus = {
+        endpoint = module.prometheus.workspace_prometheus_endpoint
+      }
+    }
     clusterArn = module.eks.cluster_arn
-    awsRegion  = data.aws_region.this.id
-    roleArn    = module.open_telemetry_iam_role.arn
-    secretsKey = aws_secretsmanager_secret.open_telemetry.id
   })
 
   depends_on = [aws_eks_capability.argocd]
 }
 
 module "open_telemetry_iam_role" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role"
-  version = "~> 6.0"
+  source = "terraform-aws-modules/eks-pod-identity/aws"
 
   name            = "${var.project_name}-open-telemetry"
   use_name_prefix = false
-  description     = "TF: IAM role to be assumed by External Secrets Operator to access the secrets of Open Telemetry."
+  description     = "TF: IAM role to be assumed by External Secrets Operator to send metrics to AMP."
 
-  trust_policy_permissions = {
-    AllowExternalSecrets = {
-      effect = "Allow"
-      actions = [
-        "sts:AssumeRole",
-        "sts:TagSession"
-      ]
-      principals = [{
-        type        = "AWS"
-        identifiers = [module.external_secrets_iam_role.iam_role_arn]
-      }]
+  trust_policy_conditions = [
+    {
+      test     = "StringLike"
+      variable = "aws:SourceArn"
+      values   = [module.eks.cluster_arn]
+    }
+  ]
+
+  associations = {
+    open-telemetry-metrics-collector = {
+      cluster_name    = module.eks.cluster_name
+      namespace       = "open-telemetry"
+      service_account = "open-telemetry-metrics-collector"
     }
   }
 
-  create_inline_policy = true
-  inline_policy_permissions = {
-    AllowSecretsManager = {
-      sid       = "AllowReadSecretsManager"
+  attach_custom_policy      = true
+  custom_policy_description = "TF: IAM policy for OpenTelemetry collectors to send metrics to AMP."
+  policy_statements = [
+    {
+      sid       = "AllowPrometheusRemoteWrite"
       effect    = "Allow"
-      actions   = ["secretsmanager:GetSecretValue", "secretsmanager:DescribeSecret"]
-      resources = [aws_secretsmanager_secret.open_telemetry.arn]
-    }
-  }
+      actions   = ["aps:RemoteWrite"]
+      resources = [module.prometheus.workspace_arn]
+    },
+  ]
 }
